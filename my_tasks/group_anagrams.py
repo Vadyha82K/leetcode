207dce88a201bcded7d98d43399f4006f3e84fd1
vadyha82k <konovalov-vadim2008@yandex.ru>
2025-04-10 00:10:50 +0300

    group_anagrams

--- my_tasks/group_anagrams.py
+++ my_tasks/group_anagrams.py
@@ -7,5 +7,16 @@ class Solution:
 
         for word in strs:
             counts = [0] * 26  #Так как в английском алфавите 26 букв, а в задании указано, что буквы только строчные
+            
             for char in word:
-                
+                counts[ord(char) - ord("a")] += 1
+
+            counts = tuple(counts)
+            if counts not in result:
+                result[counts] = []
+            result[counts].append(word)
+        
+        return list(result.values())
+
+sol = Solution()
+print(sol.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
